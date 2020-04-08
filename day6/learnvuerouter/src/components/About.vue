57fed5777e073d59dad07a6b8b7869a6c62daaf2
2268849815 <2268849815@qq.com>
2020-04-08 13:06:16 +0800

    learnvue

---
+++ day6/learnvuerouter/src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>我是关于</h2>
+</template>
+
+<script>
+export default {
+  name: "About",
+  created() {
+    console.log('created')
+    // document.title = '关于'
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ day7/tabbar/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <MainTabBar/>
+  </div>
+</template>
+<script>
+import MainTabBar from './components/MainTabBar'
+export default {
+  components: {
+    MainTabBar
+  }
+}
+</script>
+
+<style scoped>
+@import "./assets/css/base.css";
+
+</style>
