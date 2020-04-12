f062a01cbfb26988868d90c2fa3dc0611a4ecefe
2268849815 <2268849815@qq.com>
2020-04-12 22:48:10 +0800

    1

---
+++ day9/learnaxios/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
+
+axios({
+  url: 'http://123.207.32.32:8000/home/multidata',
+  method: 'get'
+}).then(res => {
+  console.log(res)
+})
+
+axios.post()
